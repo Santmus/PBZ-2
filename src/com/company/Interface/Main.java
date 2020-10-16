c82b80edb8badaaf150c89b31b7de39a387c4030
Santmus <stalker12345600@icloud.com>
2020-10-16 13:45:05 +0300

    added SystemInterface enter function

--- src/com/company/Interface/Main.java
+++ src/com/company/Interface/Main.java
@@ -1,8 +1,22 @@
 package com.company.Interface;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static SystemInterface systemInterface;
+
     public static void main(String[] args) {
-	// write your code here
+        authorization();
     }
+
+   public static void authorization(){
+        Scanner cout = new Scanner(System.in);
+        System.out.println("Введите свое ФИО:");
+        String str = cout.nextLine();
+        systemInterface = new SystemInterface();
+        systemInterface.begin(str);
+   }
+
+
 }
