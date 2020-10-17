fb070491eac0cfbd37f42a7d592c9f2c7e733fa7
Santmus <stalker12345600@icloud.com>
2020-10-17 06:55:29 +0300

    edit Command and Correspodent

--- src/com/company/DataBase/Command.java
+++ src/com/company/DataBase/Command.java
@@ -1,6 +1,8 @@
 package com.company.DataBase;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Command {
     private String number_command;
@@ -27,27 +29,55 @@ public class Command {
         return responsible_for_implementation;
     }
 
+    public void setResponsible_for_implementation(String responsible_for_implementation) {
+        this.responsible_for_implementation = responsible_for_implementation;
+    }
+
     public Date getDate_command() {
         return date_command;
+    }
+
+    public void setDate_command(Date date_command) {
+        this.date_command = date_command;
     }
 
     public Date getDate_performance_event() {
         return date_performance_event;
     }
 
+    public void setDate_performance_event(Date date_performance_event) {
+        this.date_performance_event = date_performance_event;
+    }
+
     public String getCheck_date_performance_event() {
         return check_date_performance_event;
+    }
+
+    public void setCheck_date_performance_event(String check_date_performance_event) {
+        this.check_date_performance_event = check_date_performance_event;
     }
 
     public String getContent_command() {
         return content_command;
     }
 
+    public void setContent_command(String content_command) {
+        this.content_command = content_command;
+    }
+
     public String getEvent_command() {
         return event_command;
     }
 
+    public void setEvent_command(String event_command) {
+        this.event_command = event_command;
+    }
+
     public String getNumber_command() {
         return number_command;
+    }
+
+    public void setNumber_command(String number_command) {
+        this.number_command = number_command;
     }
 }
--- src/com/company/DataBase/Correspodent.java
+++ src/com/company/DataBase/Correspodent.java
@@ -16,12 +16,23 @@ public class Correspodent {
         return name_subdivision;
     }
 
+    public void setName_subdivision(String name_subdivision) {
+        this.name_subdivision = name_subdivision;
+    }
+
     public String getPosition_correspodent() {
         return position_correspodent;
+    }
+
+    public void setPosition_correspodent(String position_correspodent) {
+        this.position_correspodent = position_correspodent;
     }
 
     public String getSurname_name() {
         return surname_name;
     }
 
+    public void setSurname_name(String surname_name) {
+        this.surname_name = surname_name;
+    }
 }
