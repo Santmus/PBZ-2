d7b164ce7d7fdae7db80cddba3f659ab77bc0a7c
Santmus <stalker12345600@icloud.com>
2020-10-17 07:21:28 +0300

    added function authorization

--- src/com/company/DataBase/Const.java
+++ src/com/company/DataBase/Const.java
@@ -1,4 +1,4 @@
-package sample.database;
+package com.company.DataBase;
 
 public class Const {
 
--- src/com/company/Interface/Main.java
+++ src/com/company/Interface/Main.java
@@ -12,10 +12,21 @@ public class Main {
 
    public static void authorization(){
         Scanner cout = new Scanner(System.in);
+        Scanner passw = new Scanner(System.in);
         System.out.println("Введите свое ФИО:");
         String str = cout.nextLine();
+        System.out.println("Введите пароль:");
+        String passwStr = passw.nextLine();
+       checkAuthorization(str,passwStr);
         systemInterface = new SystemInterface();
         systemInterface.begin(str);
+   }
+
+    public static void checkAuthorization(String str, String passwStr) {
+        if((str.length() <= 3 || str.length() > 60) || (passwStr.length() <= 6)){
+            System.out.println("Имя пользователя или пароль неверны. Попробуйте еще раз");
+            authorization();
+        }
    }
 
 
